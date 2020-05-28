c6164b02529e16585580b121e2abe257156067c7
kimiaki704 <kimiaki704@gmail.com>
2020-05-29 02:08:37 +0900

    add InstantiatableExtension

---
+++ RxSwiftTraining/InstantiatableExtension.swift
@@ -0,0 +1,27 @@
+//
+//  InstantiatableExtension.swift
+//  RxSwiftTraining
+//
+//  Created by 鈴木 公章 on 2020/05/29.
+//  Copyright © 2020 kimioman. All rights reserved.
+//
+
+import UIKit
+
+extension Instantiatable {
+    public static var storyboardName: String {
+        String(describing: self)
+    }
+
+    public static var bundle: Bundle? {
+        Bundle(for: self)
+    }
+}
+
+extension Instantiatable where Self: UIViewController {
+    public static func instantiate() -> Self {
+        let storyboard = UIStoryboard(name: storyboardName, bundle: bundle)
+        // swiftlint:disable:next force_cast
+        return storyboard.instantiateViewController(withIdentifier: storyboardName) as! Self
+    }
+}
