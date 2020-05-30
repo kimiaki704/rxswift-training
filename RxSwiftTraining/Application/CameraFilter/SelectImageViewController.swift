c57c5ad3ac2f50d4dbefeaa17f793b75049264d3
kimiaki704 <kimiaki704@gmail.com>
2020-05-30 21:32:42 +0900

    add SelectImageViewController

---
+++ RxSwiftTraining/Application/CameraFilter/SelectImageViewController.swift
@@ -0,0 +1,11 @@
+//
+//  SelectImageViewController.swift
+//  RxSwiftTraining
+//
+//  Created by 鈴木 公章 on 2020/05/30.
+//  Copyright © 2020 kimioman. All rights reserved.
+//
+
+import UIKit
+
+final class SelectImageViewController: UIViewController, Instantiatable {}
