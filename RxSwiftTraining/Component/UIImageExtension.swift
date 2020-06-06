a9b20ee859fa3b8693b668a9433ff1e41561f932
kimiaki704 <kimiaki704@gmail.com>
2020-06-06 21:31:35 +0900

    add UIImageExtension.swift

---
+++ RxSwiftTraining/Component/UIImageExtension.swift
@@ -0,0 +1,26 @@
+//
+//  UIImage+Extension.swift
+//  RxSwiftTraining
+//
+//  Created by 鈴木 公章 on 2020/06/06.
+//  Copyright © 2020 kimioman. All rights reserved.
+//
+
+import UIKit
+import CoreImage
+
+extension UIImage {
+    func applyFilter(completion: @escaping ((UIImage) -> ())) {
+        let context = CIContext()
+        let filter = CIFilter(name: "CICMYKHalftone")
+        filter!.setValue(5.0, forKey: kCIInputWidthKey)
+        if let sourceImage = CIImage(image: self) {
+            filter!.setValue(sourceImage, forKey: kCIInputImageKey)
+            guard let outputImage = filter!.outputImage else { return }
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                let processedImage = UIImage(cgImage: cgimg, scale: self.scale, orientation: self.imageOrientation)
+                completion(processedImage)
+            }
+        }
+    }
+}
