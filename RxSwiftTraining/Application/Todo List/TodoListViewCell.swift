4e5b51774d364d82e158e168bb6f0b6bafed1b85
kimiaki704 <kimiaki704@gmail.com>
2020-06-14 23:23:57 +0900

    add TodoListViewCell

---
+++ RxSwiftTraining/Application/Todo List/TodoListViewCell.swift
@@ -0,0 +1,11 @@
+//
+//  TodoListViewCell.swift
+//  RxSwiftTraining
+//
+//  Created by 鈴木 公章 on 2020/06/14.
+//  Copyright © 2020 kimioman. All rights reserved.
+//
+
+import UIKit
+
+final class TodoListViewCell: UICollectionViewCell, NibLoadableView, ReusableView {}
