b386bcd5bba01416402a2d4f9aa67c5f22985dd9
kimiaki704 <kimiaki704@gmail.com>
2020-06-14 23:29:46 +0900

    update TodoListViewController procces

--- RxSwiftTraining/Application/Todo List/TodoListViewController.swift
+++ RxSwiftTraining/Application/Todo List/TodoListViewController.swift
@@ -8,4 +8,46 @@
 
 import UIKit
 
-final class TodoListViewController: UIViewController, Instantiatable {}
+final class TodoListViewController: UIViewController, Instantiatable {
+    @IBOutlet private weak var segmentedControl: UISegmentedControl!
+    @IBOutlet private weak var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+
+    private func setup() {
+        title = "TodoList"
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        let navButton = UIButton()
+        navButton.setImage(UIImage(systemName: "plus"), for: .normal)
+        navButton.sizeToFit()
+        navButton.tintColor = .systemBlue
+        navButton.addTarget(self, action: #selector(rightNavItemButtonTapped(_:)), for: .touchUpInside)
+
+        let rightNavItem = UIBarButtonItem(customView: navButton)
+        navigationItem.rightBarButtonItem = rightNavItem
+
+        collectionView.dataSource = self
+        collectionView.register(TodoListViewCell.self)
+        collectionView.compositionalLayout(itemWidthDimension: .fractionalWidth(1.0),
+                                           itemHeightDimension: .absolute(50))
+    }
+
+    @objc private func rightNavItemButtonTapped(_ sender: UIButton) {}
+}
+
+extension TodoListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: TodoListViewCell = collectionView.dequeueReusableCell(forIndexPath: indexPath) as TodoListViewCell
+        return cell
+    }
+
+
+}
